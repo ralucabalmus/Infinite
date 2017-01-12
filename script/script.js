56b574ecd13bd7b5d4bdec2feae118398ecb34a2
ralucabalmus <raluca.balmus@yahoo.com>
2017-01-12 15:37:11 +0200

    - new contact icons , new container background, scroll up button

--- script/script.js
+++ script/script.js
@@ -8,12 +8,40 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+function pageScroll() {
+	window.scrollTo(0,0); 
+    scrolldelay = setTimeout('pageScroll()',200);
+	clearTimeout(scrolldelay);
+}
 window.onscroll = function () {
     console.log(window.pageYOffset);
-    var nav = document.getElementById('second-content');
-    if ( window.pageYOffset > 100 ) {
-        nav.classList.add("upContent");
-    } else {
-        nav.classList.remove("upContent");
+    var firstNav = document.getElementById('second-content');
+	var secondNav = document.getElementById('third-content');
+	var thirdNav = document.getElementById('fourth-content');
+	
+	var removeClass = [ firstNav, secondNav];
+    if ( window.pageYOffset > 50) {
+        firstNav.classList.add("upContent");
+    }
+	else{
+		firstNav.classList.remove("upContent");
+  }
+	if ( window.pageYOffset > 200 ) {
+        secondNav.classList.add("upContent");
+    }
+	else{
+        secondNav.classList.remove("upContent");
+    }
+	if ( window.pageYOffset > 500 ) {
+        thirdNav.classList.add("upContent");
+    }
+	else{
+        thirdNav.classList.remove("upContent");
+    }
+	if ( window.pageYOffset < 320 ) {
+       document.getElementById("scrollbtn").style.display = "none";
+    }
+	else{
+        document.getElementById("scrollbtn").style.display = "block";
     }
 }
